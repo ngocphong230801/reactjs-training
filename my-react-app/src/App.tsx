e859a8b80f87a5f3c34184602f04145260b3e80e
ngocphong230801 <nnphong962015@gmail.com>
2023-11-24 14:54:43 +0700

    Make JSX Todolist examples

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,17 +1,35 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
-export default function Avatar() {
-  const avatar = 'https://i.ibb.co/THVHD48/book-2.jpg';
-  const description = 'Gregorio Y. Zara';
+export default function TodoList() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const img = 'https://i.ibb.co/xqmT7qQ/Screenshot-2.png';
+  const description = 'Working hard everytime'
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  function formatDate(date: Date) {
+    return new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(date);
+  }
+
+  function formatTime(date: Date) {
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 
   return (
     <div>
-
-      <img
-        className="avatar"
-        src= {avatar}
-        alt= {description}
-      />
+      <h1>To Do List for {formatDate(currentTime)}</h1>
+      <p> It's now: {formatTime(currentTime)}</p>
+      <img src= {img} alt= {description} />
     </div>
   );
 }
