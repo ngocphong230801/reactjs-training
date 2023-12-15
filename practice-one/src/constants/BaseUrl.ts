2ad14d2e8c48f4f0fe2d4f5af7ca5e5074ec9d62
ngocphong230801 <nnphong962015@gmail.com>
2023-12-15 14:50:15 +0700

    Init code structure

---
+++ practice-one/src/constants/BaseUrl.ts
@@ -0,0 +1,1 @@
+const BASE_URL = "https://657bfea7394ca9e4af152952.mockapi.io/api/students/students";
---
+++ practice-one/src/types/Student/index.ts
@@ -0,0 +1,10 @@
+export interface CustomStudent {
+    id: string;
+    name: string;
+    studentID: number;
+    email: string;
+    class: string;
+    gender: boolean;
+    phone: number;
+    password: string;
+}
