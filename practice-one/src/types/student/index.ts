515a25336b396df303b2caadb7c1c75099011fe8
ngocphong230801 <nnphong962015@gmail.com>
2023-12-19 09:31:58 +0700

    Add student folder

---
+++ practice-one/src/types/student/index.ts
@@ -0,0 +1,9 @@
+export interface studentProfile {
+    name: string;
+    studentID: number;
+    email: string;
+    class: string;
+    gender: boolean;
+    phone: number;
+    password: string;
+}
