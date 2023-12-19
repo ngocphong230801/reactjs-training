23b548930e17f71a5a6d8ad170b64125dd0ff66f
ngocphong230801 <nnphong962015@gmail.com>
2023-12-19 11:31:45 +0700

    Update folder types

--- practice-one/src/types/heading.ts
+++ practice-one/src/types/heading.ts
@@ -1,9 +1,1 @@
-export interface StudentProfile {
-    name: string;
-    studentID: number;
-    email: string;
-    class: string;
-    gender: boolean;
-    phone: number;
-    password: string;
-}
+export type THeading = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
--- practice-one/src/types/studentprofile.ts
+++ practice-one/src/types/studentprofile.ts
@@ -1,1 +1,9 @@
-export type THeading = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export interface StudentProfile {
+    name: string;
+    studentID: number;
+    email: string;
+    class: string;
+    gender: boolean;
+    phone: number;
+    password: string;
+}
